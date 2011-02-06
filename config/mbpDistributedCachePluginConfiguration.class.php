4b55425daaade0208e7020494b05853f953cb866
moovando <moovando@ee427ae8-e902-0410-961c-c3ed070cd9f9>
2011-02-06 17:29:23 -0600

    - initial plugin structure
    git-svn-id: http://svn.symfony-project.com/plugins/mbpDistributedCachePlugin/trunk@31999 ee427ae8-e902-0410-961c-c3ed070cd9f9

---
+++ config/mbpDistributedCachePluginConfiguration.class.php
@@ -0,0 +1,21 @@
+<?php
+
+/**
+ * mbpDistributedCachePlugin configuration.
+ * 
+ * @package     mbpDistributedCachePlugin
+ * @subpackage  config
+ * @author      Burkhard Reffeling <burkhard.reffeling@gmail.com>
+ * @version     SVN: $Id: PluginConfiguration.class.php 17207 2009-04-10 15:36:26Z Kris.Wallsmith $
+ */
+class mbpDistributedCachePluginConfiguration extends sfPluginConfiguration
+{
+  const VERSION = '1.0.0-DEV';
+
+  /**
+   * @see sfPluginConfiguration
+   */
+  public function initialize()
+  {
+  }
+}
